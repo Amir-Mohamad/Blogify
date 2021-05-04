9ef875326c6fb1b0332c8dccf5e0d5f21392b028
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-05-04 17:26:27 +0430

    feat: Category Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return f'{self.title} created'
